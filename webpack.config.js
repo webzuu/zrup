9f04b8dd3be2de745ae99bb5438a6f8a2d688d93
Szczepan Hołyszewski <rulatir@wp.pl>
2020-11-24 07:59:07 +0100

    Fake webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+// This tool isn't actually built using webpack.
+// This file only exists to help JetBrains IDEs recognize module aliases.
+
+const path = require('path');
+
+const moduleAliases = require('./package.json')._moduleAliases;
+const alias = {};
+
+for (const key in Object.getOwnPropertyNames(moduleAliases)) {
+    alias[key] = path.resolve(__dirname, moduleAliases[key]);
+}
+
+module.exports = {
+    resolve: { alias },
+};
