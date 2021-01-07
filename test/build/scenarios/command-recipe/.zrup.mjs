3e3d662e7dc9bf1f8a6c92ee64b1656c2dcf61e9
Szczepan Hołyszewski <rulatir@wp.pl>
2021-01-08 00:16:57 +0100

    Introduce hierarchical dependency in CommandRecipe test

--- test/build/scenarios/command-recipe/.zrup.mjs
+++ test/build/scenarios/command-recipe/.zrup.mjs
@@ -2,19 +2,6 @@ import {CommandRecipe} from "../../../../build/command-recipe.js";
 
 export default function test({module, rule}) {
 
-    rule(function concatenated({depends, produces}) {
-
-        const [[target], sources] = [
-            produces('actual.txt'),
-            depends('src/input1.txt', 'src/input2.txt')
-        ];
-
-        return new CommandRecipe(({cmd, out}) => {
-            cmd("cat", ...sources);
-            out(target);
-        });
-    });
-
     rule(function fromEmpty({depends, produces}) {
 
         const [[target], sources] = [
@@ -26,5 +13,18 @@ export default function test({module, rule}) {
             cmd("cat", ...sources);
             out(target);
         })
+    });
+
+    rule(function concatenated({depends, produces}) {
+
+        const [[target], sources] = [
+            produces('actual.txt'),
+            depends('src/input1.txt', 'src/input2.txt', 'shouldBeEmpty.txt')
+        ];
+
+        return new CommandRecipe(({cmd, out}) => {
+            cmd("cat", ...sources);
+            out(target);
+        });
     });
 };
