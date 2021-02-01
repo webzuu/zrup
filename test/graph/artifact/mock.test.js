44bb5b86f47ecfcfa3df9314b9b02b235a25a0a9
Szczepan Hołyszewski <rulatir@wp.pl>
2021-02-01 09:58:17 +0100

    Greenify artifact tests

--- test/graph/artifact/mock.test.js
+++ test/graph/artifact/mock.test.js
@@ -1,18 +1,7 @@
 import chai from "chai";
 import {MockArtifact} from "../../../src/graph/artifact/mock.js";
 import {PromiseKeeper} from "../../../src/util/promise-keeper.js";
-import path from "path";
 const expect = chai.expect;
-
-
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-import {DbTesting} from "../../../src/util/testing.js";
-
-const t = new DbTesting(path.join(__dirname, '../tmp'));
 
 describe("Mock artifact", () => {
 
