f4af24d100bcb8811fd855cefd26e8de82db0418
rulatir <rulatir@wp.pl>
2024-05-12 13:13:57 +0200

    Commit docs, bump version

--- doc/.zrup.mjs
+++ doc/.zrup.mjs
@@ -6,11 +6,12 @@ const docs = async function docs(M) {
 
     M.to('typedoc', ({T})=>{
 
+        depends('zrup+tsconfig.json')
         return {
             cwd: SRC,
             cmd: [
                 'rm -rf doc/td/*',
-                '&& typedoc --tsconfig tsconfig.json --gitRevision wip --out doc/td ts/',
+                T`&& ${SRC}/node_modules/.bin/typedoc --gitRevision wip --out doc/td --entryPointStrategy expand ./ts`,
                 T`&& mkdir -p $(dirname ${resolve('internal:built')})`,
                 T`&& cp ${depends('internal:js+built')} ${produces('internal:built')}`
             ]
